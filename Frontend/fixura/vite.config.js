fb7989b237814e8a8c33c04f4d8d686e59431b65
V S Sadique <vssadique785@gmail.com>
2025-03-17 18:16:12 +0530

    correction no 1

--- Frontend/fixura/vite.config.js
+++ Frontend/fixura/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',  // Ensures Vite correctly serves assets
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
 });
